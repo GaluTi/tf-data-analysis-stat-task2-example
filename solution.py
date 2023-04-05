4bb2aff22e63bc7ef87d690fb11d9a9cfe266e2f
J.P <62064427+GaluTi@users.noreply.github.com>
2023-04-05 13:09:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import gamma
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 640884635 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    sum_x = sum(x)
+    z_1_g = gamma.ppf(alpha/2, a=len(x), loc=sum_x - len(x)/2, scale=1)
+    z_2_g = gamma.ppf(1-alpha/2, a=len(x), loc=sum_x - len(x)/2, scale=1)
+    a_g = z_1_g * 2 / (len(x) * 5**2)
+    b_g = z_2_g * 2 / (len(x) * 5**2)
+    return a_g, b_g
